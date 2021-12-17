dc3c8877f4820cc05c97fd4b401c7919bea11078
MErtanM <mertan1971@gmail.com>
2021-12-17 23:08:11 +0300

    added array

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const student1 = "Muhlise"
+const student2 = "Utku"
+const student3 = "Kemal" // tek tek veri girme yerine array ile toplu veri girilebilir
+
+const studentArray =["Muhlise", "Utku", "Kemal", "Cengiz", "Erhan", "Ertan"]
+console.log(studentArray)
+
+
+console.log(studentArray[0])
+console.log(studentArray[1])
+console.log(studentArray[5])
+
+console.log(studentArray[studentArray.length -1])
+console.log(studentArray.length)
